03577e6497ee59bad1e00ca03946db5f3320a271
GADDIR <islamovdr@yandex.ru>
2026-04-14 23:17:47 +0700

    Update Svyaznoy_NPC.c

--- Scripts/4_World/Svyaznoy_NPC.c
+++ Scripts/4_World/Svyaznoy_NPC.c
@@ -1,24 +1,35 @@
-cpp
 modded class MissionServer
 {
-    protected PlayerBase m_SvyaznoyEntity;
+    // 1. Меняем тип переменной на наш кастомный класс
+    protected Svyaznoy_NPC m_SvyaznoyEntity; 
+
     override void OnInit()
     {
         super.OnInit();
+
         vector svyaznoyPos = "319.088 284.23 246.283"; 
         vector svyaznoyOri = "180 0 0"; 
-        m_SvyaznoyEntity = PlayerBase.Cast(GetGame().CreateObject("SurvivorM_Seth", svyaznoyPos, false, false, true));
+
+        // 2. ГЛАВНОЕ: Создаем объект через класс Svyaznoy_NPC. 
+        // Движок подтянет внешность Seth, если класс правильно прописан в config.cpp,
+        // либо создаст сущность, которая немедленно инициализирует m_Brain.
+        m_SvyaznoyEntity = Svyaznoy_NPC.Cast(GetGame().CreateObject("Svyaznoy_NPC", svyaznoyPos, false, false, true));
+
         if (m_SvyaznoyEntity)
         {
             m_SvyaznoyEntity.SetPosition(svyaznoyPos);
             m_SvyaznoyEntity.SetOrientation(svyaznoyOri);
-            m_SvyaznoyEntity.SetAllowDamage(false); 
+            
+            // Экипировка ветерана (Манифест ТТсКО)
             m_SvyaznoyEntity.GetInventory().CreateInInventory("Gorka_Pants_Summer");
             m_SvyaznoyEntity.GetInventory().CreateInInventory("Gorka_Jacket_Summer");
             m_SvyaznoyEntity.GetInventory().CreateInInventory("MilitaryBoots_Black");
             m_SvyaznoyEntity.GetInventory().CreateInInventory("PersonalRadio"); 
+
+            m_SvyaznoyEntity.SetAllowDamage(false); 
             m_SvyaznoyEntity.GetInputController().SetDisabled(true); 
-            Print("[Связной]: Алексей Некрасов заступил на пост 319/284/246. Эфир чист.");
+            
+            Print("[СВЯЗНОЙ]: Алексей Некрасов заступил на пост. Класс NPC инициализирован.");
         }
     }
 };
