b74809f2beed286bc8b7db1427d67876480d9515
GADDIR <islamovdr@yandex.ru>
2026-04-14 23:16:11 +0700

    Update missionServer.c

--- Scripts/5_Mission/missionServer.c
+++ Scripts/5_Mission/missionServer.c
@@ -1,6 +1,6 @@
 modded class MissionServer
 {
-    protected Svyaznoy_NPC m_SvyaznoyEntity; // Используем наш класс вместо PlayerBase
+    protected Svyaznoy_NPC m_SvyaznoyEntity; 
 
     override void OnInit()
     {
@@ -9,9 +9,13 @@ modded class MissionServer
         vector svyaznoyPos = "319.088 284.23 246.283"; 
         vector svyaznoyOri = "180 0 0"; 
 
-        // СПАВН: Создаем именно Svyaznoy_NPC. "SurvivorM_Seth" — это визуальный скин (внешность).
-        m_SvyaznoyEntity = Svyaznoy_NPC.Cast(GetGame().CreateObject("SurvivorM_Seth", svyaznoyPos, false, false, true));
+        // ИСПОЛЬЗУЕМ КЛАСС NPC (Он наследует внешность внутри себя или через config)
+        m_SvyaznoyEntity = Svyaznoy_NPC.Cast(GetGame().CreateObject("Svyaznoy_NPC", svyaznoyPos, false, false, true));
 
+        // Если config.cpp еще не настроен, используй "SurvivorM_Seth", 
+        // но тогда в 4_World/Entities/ManBase/PlayerBase.c 
+        // должна быть логика из нашего "Манифеста 1.1.0_INTEGRATED"
+        
         if (m_SvyaznoyEntity)
         {
             m_SvyaznoyEntity.SetPosition(svyaznoyPos);
@@ -23,9 +27,8 @@ modded class MissionServer
             m_SvyaznoyEntity.GetInventory().CreateInInventory("MilitaryBoots_Black");
             m_SvyaznoyEntity.GetInventory().CreateInInventory("PersonalRadio"); 
 
-            // Настройка состояния
-            m_SvyaznoyEntity.SetAllowDamage(false); // Пока идет отладка — бессмертен
-            m_SvyaznoyEntity.GetInputController().SetDisabled(true); // Запрет внешнего управления
+            m_SvyaznoyEntity.SetAllowDamage(false); 
+            m_SvyaznoyEntity.GetInputController().SetDisabled(true); 
             
             Print("[СВЯЗНОЙ]: Алексей Некрасов заступил на пост. Разум и снаряжение синхронизированы.");
         }
