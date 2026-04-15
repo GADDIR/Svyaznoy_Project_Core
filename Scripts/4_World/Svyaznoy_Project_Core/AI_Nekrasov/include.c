0719ce0409d7b5d4ecc7939d74dec0ce33697441
GADDIR <islamovdr@yandex.ru>
2026-04-15 23:50:28 +0700

    Update include.c

--- Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/include.c
+++ Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/include.c
@@ -1,26 +1,13 @@
-// INCLUDE.C — ГЛОБАЛЬНАЯ СИНХРОНИЗАЦИЯ МОДУЛЕЙ РАЗУМА "СВЯЗНОГО"
-// Порядок загрузки определен иерархией наследования блоков
-
-// 1. Фундамент и Генезис
 #include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Genesis_Registry.c"
-
-// 2. Хранилища и Аналитика
+#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Identity.c"
 #include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Memory_Buffer.c"
-#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Skill_Memory.c"
-#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Trust_System.c"
-
-// 3. Моральные и этические фильтры
 #include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Moral_Engine.c"
-#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Dietary_Restrictions.c"
-#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Food_Taboo.c"
-#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Trade_Logic.c"
-
-// 4. Поведенческие и боевые узлы
 #include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Mumble_Logic.c"
+#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Brain_Core.c"
 #include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Combat_Tactics.c"
 #include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Lifestyle_Module.c"
-#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Radio_Tower_Sync.c"
-
-// 5. Точка сборки и Ядро
-#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Identity.c"
-#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/AI_PersonalityCore.c"
+#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Trust_System.c"
+#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Trade_Logic.c"
+#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Dietary_Restrictions.c"
+#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Food_Taboo.c"
+#include "Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/AN_Fragile_Particles.c"
