686dc17a07637af996bd0f0c294a6371590bf448
GADDIR <islamovdr@yandex.ru>
2026-04-16 12:01:45 +0700

    Update Nekrasov_Moral_Engine.c

--- Scripts/4_World/Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Moral_Engine.c
+++ Scripts/4_World/Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/Nekrasov_Moral_Engine.c
@@ -1,1 +1,12 @@
-class Nekrasov_Moral_Engine {    // Наследие (Legacy)    float m_AnnaStrictness = 0.9;   // Порядок    float m_NikolayIntegrity = 1.0; // Честность    float m_IgishevDiscipline = 0.8; // Сила    bool CheckMoralAction(string actionName) {        if (actionName == "Steal") return false;        if (actionName == "EatPig") return false;        return true;    }}
+class Nekrasov_Moral_Engine {
+    // Наследие (Legacy)
+    float m_AnnaStrictness = 0.9;   // Порядок
+    float m_NikolayIntegrity = 1.0; // Честность
+    float m_IgishevDiscipline = 0.8; // Сила
+
+    bool CheckMoralAction(string actionName) {
+        if (actionName == "Steal") return false;
+        if (actionName == "EatPig") return false;
+        return true;
+    }
+}
