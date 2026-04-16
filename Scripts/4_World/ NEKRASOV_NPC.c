1127bd041d5c2aec67d5b2f00facba6e839e83a0
GADDIR <islamovdr@yandex.ru>
2026-04-16 19:27:13 +0700

    Update and rename Svyaznoy_NPC.c to  NEKRASOV_NPC.c

--- Scripts/4_World/ NEKRASOV_NPC.c
+++ Scripts/4_World/ NEKRASOV_NPC.c
@@ -1,7 +1,7 @@
 modded class MissionServer
 {
     // 1. Меняем тип переменной на наш кастомный класс
-    protected Svyaznoy_NPC m_SvyaznoyEntity; 
+    protected  NEKRASOV_NPC m_ NEKRASOVEntity; 
 
     override void OnInit()
     {
@@ -10,15 +10,15 @@ modded class MissionServer
         vector svyaznoyPos = "319.088 284.23 246.283"; 
         vector svyaznoyOri = "180 0 0"; 
 
-        // 2. ГЛАВНОЕ: Создаем объект через класс Svyaznoy_NPC. 
+        // 2. ГЛАВНОЕ: Создаем объект через класс  NEKRASOV_NPC. 
         // Движок подтянет внешность Seth, если класс правильно прописан в config.cpp,
         // либо создаст сущность, которая немедленно инициализирует m_Brain.
-        m_SvyaznoyEntity = Svyaznoy_NPC.Cast(GetGame().CreateObject("Svyaznoy_NPC", svyaznoyPos, false, false, true));
+        m_SvyaznoyEntity = Svyaznoy_NPC.Cast(GetGame().CreateObject(" NEKRASOV_NPC",  NEKRASOVPos, false, false, true));
 
         if (m_SvyaznoyEntity)
         {
-            m_SvyaznoyEntity.SetPosition(svyaznoyPos);
-            m_SvyaznoyEntity.SetOrientation(svyaznoyOri);
+            m_SvyaznoyEntity.SetPosition( NEKRASOVPos);
+            m_SvyaznoyEntity.SetOrientation( NEKRASOVOri);
             
             // Экипировка ветерана (Манифест ТТсКО)
             m_SvyaznoyEntity.GetInventory().CreateInInventory("Gorka_Pants_Summer");
