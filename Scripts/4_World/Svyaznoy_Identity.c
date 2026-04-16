b38556157cfa40ab829791464ebc3571637d490f
GADDIR <islamovdr@yandex.ru>
2026-04-16 19:17:40 +0700

    Update Svyaznoy_Identity.c

--- Scripts/4_World/Svyaznoy_Identity.c
+++ Scripts/4_World/Svyaznoy_Identity.c
@@ -25,7 +25,7 @@ modded class PlayerBase
         this.SetObjectTexture(0, "dz\\characters\\heads\\m_mirek\\data\\m_mirek_co.paa");
 
         // 2. Идентификация личности
-        this.SetPlayerName("НЕКРАСОВ АЛЕКСЕЙ НИКАЛАЕВИЧ (Связной)");
+        this.SetPlayerName("НЕКРАСОВ АЛЕКСЕЙ НИКОЛАЕВИЧ (Связной)");
 
         // 3. Комплектация (Гарант снаряжения согласно протоколу)
         SetupMaverickPhysicalBody();
