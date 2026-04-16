e29400cfc885ce4e07f4e6bef8396621bb25d4a0
GADDIR <islamovdr@yandex.ru>
2026-04-16 12:40:38 +0700

    Update and rename NEKRASOV_Core_Brain.1.c to NEKRASOV_Core_Brain_1.c

--- Scripts/4_World/Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/NEKRASOV_Core_Brain_1.c
+++ Scripts/4_World/Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/NEKRASOV_Core_Brain_1.c
@@ -1,29 +1,29 @@
 // ЦЕНТРАЛЬНОЕ ЯДРО ИИ "СВЯЗНОЙ" (CORE BRAIN)
 // Интеграция Блоков №1 - №9 и Золотого Стандарта
 
-class NEKRASOV_Core_Brain.1
+class NEKRASOV_Core_Brain_1
 {
     // Ссылки на подсистемы памяти
-    private ref AN_MemoryController    m_Memory
-    private ref AN_SocialBrain          m_Social
-    private ref AN_ResourceLoyalty      m_Loyalty
-    private ref AN_PersistentMemory     m_Persistent
+    private ref NEKRASOV_MemoryController    m_Memory
+    private ref NEKRASOV_SocialBrain          m_Social
+    private ref NEKRASOV_ResourceLoyalty      m_Loyalty
+    private ref NEKRASOV_PersistentMemory     m_Persistent
     
     // Владелец мозга
     private PlayerBase m_Nekrasov
 
-    void AN_Core_Brain(PlayerBase player)
+    void NEKRASOV_Core_Brain_1(PlayerBase player)
     {
         m_Nekrasov = player
         
         // Инициализация всех слоев
-        m_Memory     = new AN_MemoryController(player)
-        m_Social     = new AN_SocialBrain()
-        m_Loyalty    = new AN_ResourceLoyalty()
-        m_Persistent = new AN_PersistentMemory()
+        m_Memory     = new NEKRASOV_MemoryController(player)
+        m_Social     = new NEKRASOV_SocialBrain()
+        m_Loyalty    = new NEKRASOV_ResourceLoyalty()
+        m_Persistent = new NEKRASOV_PersistentMemory()
         
         // Применение Генезиса 1982 при рождении
-        AN_Genesis_1982.ApplyChildhoodTraits(player)
+        NEKRASOV_Genesis_1982.ApplyChildhoodTraits(player)
     }
 
     // ГЛАВНЫЙ ЦИКЛ МЫШЛЕНИЯ
@@ -33,21 +33,21 @@ class NEKRASOV_Core_Brain.1
         m_Memory.OnUpdate(timeslice, m_Nekrasov)
 
         // 2. Проверка инстинктов (Протокол №1 из Блока 5)
-        AN_MilitaryService_2001.ExecuteTacticalOverride(m_Nekrasov)
+        AN_MilitaryService_2001.ExecuteTacticalOverride(m_NEKRASOV)
 
         // 3. Автономный аудит (Блок 6)
-        AN_WorkExperience_2015.PerformInventoryAudit(m_Nekrasov)
+        AN_WorkExperience_2015.PerformInventoryAudit(m_NEKRASOV)
     }
 
     // ПРИНЯТИЕ РЕШЕНИЙ (Фильтр через Золотой Стандарт)
     bool CanIAction(string actionType)
     {
         // Сверяемся с "Конституцией" Некрасова
-        if (!AN_GoldenStandard.ValidateAction(m_Nekrasov, actionType))
+        if (!NEKRASOV_GoldenStandard.ValidateAction(m_NEKRASOV, actionType))
             return false
         
         // Проверка гигиены из Блока 1 (1982)
-        if (!AN_Genesis_1982.CheckFoodPurity(m_Nekrasov.GetStatDirtyHands().Get() > 0))
+        if (!NEKRASOV_Genesis_1982.CheckFoodPurity(m_NEKRASOV.GetStatDirtyHands().Get() > 0))
             return false
 
         return true
@@ -56,7 +56,7 @@ class NEKRASOV_Core_Brain.1
     // РЕАКЦИЯ НА ИГРОКА (Социальный скоринг + Прагматизм)
     void OnPlayerDetected(PlayerBase other)
     {
-        m_Social.EvaluateTarget(m_Nekrasov, other)
+        m_Social.EvaluateTarget(m_NEKRASOV, other)
         
         // Если Бактерия полезная (Блок 9), включаем ресурсную лояльность
         if (m_Social.GetSocialStatus(other.GetIdentity().GetId()) == "NEUTRAL")
