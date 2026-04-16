1b87bd84eb3f8a58494a364ec8c292a63610e9e0
GADDIR <islamovdr@yandex.ru>
2026-04-16 15:58:11 +0700

    Update and rename Nekrasov_FRAGILE_PARTICLES.C to NEKRASOV_FRAGILE_PARTICLES.C

--- Scripts/4_World/Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/NEKRASOV_FRAGILE_PARTICLES.C
+++ Scripts/4_World/Scripts/4_World/Svyaznoy_Project_Core/AI_Nekrasov/NEKRASOV_FRAGILE_PARTICLES.C
@@ -1,7 +1,7 @@
-// Nekrasov_FRAGILE_PARTICLES.C — ХРУПКИЕ ЧАСТИЦЫ ПАМЯТИ (БЛОК B)
+// NEKRASOV_FRAGILE_PARTICLES.C — ХРУПКИЕ ЧАСТИЦЫ ПАМЯТИ (БЛОК B)
 // Интеграция: Блок №1 (1982) — Лилии, Тени, Фото у Ёлки.
 
-class AN_Fragile_Particles
+class NEKRASOV_Fragile_Particles
 {
     // ГЛАВНЫЙ МЕТОД ОБРАБОТКИ ЭМОЦИОНАЛЬНЫХ ТРИГГЕРОВ
     static void ProcessEmotions(PlayerBase player, EntityAI item, float hour)
@@ -11,16 +11,16 @@ class AN_Fragile_Particles
         if (item.HasColor("Red_Yellow") || item.IsFlower())
             player.SetAggressionLevel(0.0) // Сброс агрессии
             player.SetStareDuration(10.0)  // Замирание на 10 секунд
-            Nekrasov_Mumble_Logic.Say(player, "Запах... как тогда. У отца в руках.")
+            NEKRASOV_Mumble_Logic.Say(player, "Запах... как тогда. У отца в руках.")
 
         // 2. ТЕАТР ТЕНЕЙ (Сумерки 20:00 - 22:00)
         if (hour >= 20.0 && hour <= 22.0)
             if (player.IsNearFireplace())
                 player.RequestFlashback("SHADOW_THEATER")
-                Nekrasov_Mumble_Logic.Say(player, "Птица... Мама так руки складывала... а потом всё, обрыв.")
+                NEKRASOV_Mumble_Logic.Say(player, "Птица... Мама так руки складывала... а потом всё, обрыв.")
 
         // 3. ЗАЩИТНЫЙ ТАЛИСМАН (Фото 1982 года)
-        if (!player.HasItemInInventory("AN_Photo_1982"))
+        if (!player.HasItemInInventory("NEKRASOV_Photo_1982"))
             // Если фото украдено или потеряно — режим берсерка
             player.ActivateProtocol("BERSERK_MODE") 
     }
