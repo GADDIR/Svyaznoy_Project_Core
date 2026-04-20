1ed288098db846c6e6743e94148626b535807f4e
GADDIR <islamovdr@yandex.ru>
2026-04-21 03:40:22 +0700

    Create NEKRASOV_AI_Include.c

---
+++ Scripts/4_World/NEKRASOV_AI_Include.c
@@ -0,0 +1,4 @@
+#include "Svyaznoy_Project_Core/Scripts/4_World/NEKRASOV_AI/NEKRASOV_MaverickController.c"
+#include "Svyaznoy_Project_Core/Scripts/4_World/NEKRASOV_AI/Core/NEKRASOV_Brain.c"
+#include "Svyaznoy_Project_Core/Scripts/4_World/NEKRASOV_AI/Core/NEKRASOV_Memory.c"
+// и так далее для всех файлов...
