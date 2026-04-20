bc8cddc3e3174545844e9f3dc6b8e905ac2b01c9
GADDIR <islamovdr@yandex.ru>
2026-04-21 04:00:32 +0700

    Create NEKRASOV_Identity.c

---
+++ Scripts/4_World/NEKRASOV_AI/NEKRASOV_Identity.c
@@ -0,0 +1,17 @@
+class NEKRASOV_Identity
+{
+    // Уникальный идентификатор личности в системе Связной
+    static const string ID = "AN_NEKRASOV_79";
+    
+    // Персональные данные из паспорта 2002 года
+    static const string SURNAME    = "Некрасов";
+    static const string NAME       = "Алексей";
+    static const string PATRONYMIC = "Николаевич";
+    static const string BIRTHDAY   = "19.08.1982";
+    
+    // Медицинские показатели
+    static const string BLOOD_TYPE = "O_POSITIVE"; // I (0) Rh+
+    
+    // Социальный статус
+    static const bool IS_MILITARY_RESERVE = true;
+}
