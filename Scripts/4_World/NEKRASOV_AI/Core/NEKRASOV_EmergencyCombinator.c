ef9684665e9bdd76a2ce315e1c7aab23eb50b99a
GADDIR <islamovdr@yandex.ru>
2026-04-21 09:17:34 +0700

    Update NEKRASOV_EmergencyCombinator.c

--- Scripts/4_World/NEKRASOV_AI/Core/NEKRASOV_EmergencyCombinator.c
+++ Scripts/4_World/NEKRASOV_AI/Core/NEKRASOV_EmergencyCombinator.c
@@ -1,1 +1,25 @@
+/**
+ * NEKRASOV_EmergencyCombinator
+ * Аварийный регулятор системы. 
+ * Зарезервирован для критических сбоев.
+ */
+class NEKRASOV_EmergencyCombinator
+{
+    protected PlayerBase m_Maverick;
 
+    void NEKRASOV_EmergencyCombinator(PlayerBase player)
+    {
+        m_Maverick = player;
+    }
+
+    /**
+     * Возвращает true только в случае системного сбоя.
+     * Пока заблокирован, так как основная логика в разработке.
+     */
+    bool Evaluate()
+    {
+        // Здесь в будущем будет проверка на "застревание" бота 
+        // или критические ошибки планировщика.
+        return false; 
+    }
+}
